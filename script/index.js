d96a8edd13bcca8b162ec39ce6ad70140cf65e58
Client444 <the.reformed.alloy@gmail.com>
2019-12-25 02:32:12 -0500

    Made changes to site.

--- script/index.js
+++ script/index.js
@@ -1,16 +1,30 @@
+const header = document.getElementsByTagName('header')[0];
+    let originalHeaderSize;
+const mainPage = document.getElementById('page').children;
+const footer = document.getElementsByTagName('footer')[0];
+
 const setInitialSizes = () => {
-    const header = document.getElementsByTagName('header')[0];
-    header.style.height = window.innerHeight / 10 + "px";
+    originalHeaderSize = window.innerHeight / 10 + "px";
+    header.style.height = originalHeaderSize;
     
-    const mainPage = document.getElementById('page').children;
-    
-    mainPage[0].style.height = window.innerHeight + "px";
-    mainPage[0].style.paddingTop = Number(header.style.height.split("px")[0]) + 20 + "px";
-    console.log(Number(header.style.height.split("px")[0]) + 20 + "px");
-    mainPage[1].style.height = window.innerHeight / 4 + "px";
+    mainPage[0].style.minHeight = window.innerHeight + "px";
+    mainPage[0].style.paddingTop = Number(header.style.height.split("px")[0]) + "px";
+    mainPage[1].style.minHeight = window.innerHeight / 4 + "px";
     mainPage[2].style.minHeight = window.innerHeight + "px";
+    
+    footer.style.height = window.innerHeight / 5 + "px";
 };
 
 setInitialSizes();
 
 window.onresize = setInitialSizes;
+
+const resizeHeader = (event) => {
+    if(event.target.scrollTop >= Number(header.style.height.split("px")[0])) {
+        header.style.height = originalHeaderSize / 2;
+    } else {
+        header.style.height = originalHeaderSize;
+    }
+}
+
+document.documentElement.onscroll = resizeHeader;
